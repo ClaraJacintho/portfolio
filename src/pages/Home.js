cd1612623f2d669751e1407ed2df0afbafdb2fe3
Clara Jacintho <mcmachry@gmail.com>
2019-06-24 00:02:08 -0700

    Added 'skills' CTA button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Container, Row, Col, Image} from 'react-bootstrap'
+import {Container, Row, Col, Image, Button} from 'react-bootstrap'
+import Link from 'react-router-dom/Link'
 import Picture from '../assets/petit.jpg'
 import './Home.css'
 
@@ -13,12 +14,17 @@ class Home extends React.Component {
                             <Image src={Picture}  roundedCircle className="profile"/>
                         </Col>
                         <Col lg={true}>
-                            <h1 className="title">Clara Jacintho</h1>
-                            <h2 className="description">Computer Science student</h2>
-                            <h2 className="description">Software Engineering | Machine Learning | Web Dev</h2>
                             <div className="text-center">
+                                <h1 className="title">Clara Jacintho</h1>
+                                <h2 className="description">Computer Science student</h2>
+                                <h2 className="description">Software Engineering | Machine Learning | Web Dev</h2>
+                                
                                 <a className="btn" href="https://github.com/claraJacintho" target="_blank"><i className="fab fa-github"></i></a>
                                 <a className="btn" href="https://www.linkedin.com/in/clarajacintho/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                                <h1 className="call">Check out my skills!</h1>
+                                <Button variant="light" block>
+                                    <Link to="/skills" className="cta">Skills</Link>
+                                </Button>
                             </div>
                         </Col>
                     </Row>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -3,6 +3,6 @@ import React from 'react';
 
 export const Skills = () => (
     <div>
-        <h1>Skills</h1>
+        <h1>Skills!</h1>
     </div>
 )
