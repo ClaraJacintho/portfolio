5b4e6c3a0c38beba780d0946df410bed089f660e
Clara Jacintho <mcmachry@gmail.com>
2019-07-02 00:24:43 -0700

    Added longer skills descriptions, plain

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -38,7 +38,13 @@ class Skills extends Component {
                     <Row>
                         <Col lg={true}>
                             <h1 className="subtitle" style={{color: "#072C82"}}>Software Engineering</h1>
-                            <p>I'm very good. Trust me!</p>
+                            <p>It is definitely my strongest area! I have studied it a lot in university, and have had the opportunity to apply the 
+                                concepts in many different projects. I was also able to develop this set of skills in my previous internship at SAP, 
+                                by building and maintaining HR software.</p>
+                            <p> I am most familiar with Java, with C++ a near second, having worked with these languages for many projects in school, 
+                                where I also used design patternsl. Also, since my current degree is in Computer Science and Management I have had the 
+                                opportunity to study and to put in practice many of the facets of Project Management.
+                            </p>
                         </Col>
                         <Col lg={true}>    
                             <VictoryBar horizontal data={software} barRatio={1.4} style={{
@@ -62,7 +68,12 @@ class Skills extends Component {
                     <Row>
                         <Col lg={true}>
                             <h1 className="subtitle" style={{color: "#076D82"}}>Machine Learning</h1>
-                            <p>Good, not amazing</p>
+                            <p>Probably my two favourite areas! I have learnt a lot about them by myself, 
+                            using resources found online, but I have also had some courses at my university, 
+                            especially data science. More recently, I developed a machine learning/computer vision 
+                            project for 2 months in partnership with Boise State University.</p>
+                            <p>I have experience using Python, Keras and TensorFlow for machine learning and R for data science.
+                            </p>
                         </Col>
                         <Col lg={true}>
                             <VictoryBar horizontal data={ml} barRatio={1.4} style={{
@@ -86,7 +97,13 @@ class Skills extends Component {
                     <Row>
                         <Col lg={true}>
                             <h1 className="subtitle" style={{color: "#06786C"}}>Web development</h1>
-                            <p>Basically a noob!</p>
+                            <p>My newest skill! While I am quite new to this part of the computer science world, 
+                                I already love it. Being able to build something and share with friends and family 
+                                is an incredible feeling! The only experience I have so far is this website and the projects 
+                                and lessons on FreeCodeCamp, but I am already planning some new projects to continue to develop my abilities.</p>
+
+                            <p>I am most familiar with JavaScript (React, react-bootstrap), HTML/CSS and GitHub pages.
+                            </p>
                         </Col>
                         <Col lg={true}>
                             <VictoryBar horizontal data={webdev} barRatio={1.4} style={{
