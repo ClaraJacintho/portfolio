70ece3aa668340c770492d569e465c87aa289479
Clara Jacintho <mcmachry@gmail.com>
2019-07-03 23:22:31 -0700

    Readded education page, added timeline

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,8 +1,80 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { ReactComponent as PET } from "../assets/pet.svg"
+import { ReactComponent as UCSD } from "../assets/ucsd.svg"
+import { ReactComponent as UFGRS } from "../assets/ufrgs.svg"
+import { ReactComponent as UM } from "../assets/um.svg"
+import { ReactComponent as SAP } from "../assets/sap.svg"
+
 
 
 export const Education = () => (
-    <div>
-        <h1>Education</h1>
+    <div className="background top">
+        <div> ⠀ </div>
+        <div className="page" style={{background: "#E8DCCA"}}>
+        <VerticalTimeline>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2011 - present"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon={<UCSD/>}
+            >
+                <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                <p>
+                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2010 - 2011"
+                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                icon={<UM/>}
+            >
+                <h3 className="vertical-timeline-element-title">Art Director</h3>
+                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <p>
+                Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2008 - 2010"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon={<SAP/>}
+            >
+                <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                <p>
+                User Experience, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2006 - 2008"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon={<PET/>}
+            >
+                <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <p>
+                User Experience, Visual Design
+                </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--education"
+                date="2002 - 2006"
+                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                icon={<UFGRS/>}
+            >
+                <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+                <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+                <p>
+                Creative Direction, Visual Design
+                </p>
+            </VerticalTimelineElement>
+        </VerticalTimeline>
+        </div>
     </div>
 )
